10a24905c31510980a1aec3ddc71ff3b07288345
jbhdeconinck <jbhdeconinck@gmail.com>
2015-11-24 11:34:39 +0000

    show restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,4 +16,9 @@ class RestaurantsController < ApplicationController
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
