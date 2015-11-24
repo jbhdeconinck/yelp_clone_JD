66f8774cf4ad66f3a9f9b3588356c98e356202ca
mongolianprincess <ayc.yang@gmail.com>
2015-11-24 18:02:02 +0000

    Added validation for rating to be maximum 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Review, type: :model do
   it { is_expected.to belong_to :restaurant }
+
+  it 'is not valid with a rating greater than 5' do
+    restaurant = Restaurant.create(name: 'KFC')
+    review = restaurant.reviews.create(thoughts: 'spectacular', rating: 6)
+    expect(review).to have(1).error_on(:rating)
+    expect(review).not_to be_valid
+  end
 end
